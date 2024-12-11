1a4bd5d1cec045c17bd9343167a5f887964d4b54
aya2003A <101410399+aya2003A@users.noreply.github.com>
2024-12-11 13:24:24 +0200

    fix the problem Gemini endpoint

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Config:
     MONGO_URI = 'mongodb+srv://abdullahmohamed1047:HuexVTFWCHGP8wXU@cluster0.ydoaic1.mongodb.net/'
     SENDER_EMAIL = 'abdullah.mohamed1047@gmail.com'
     SENDER_PASSWORD = 'ozwd vhmw aada pxnq'
-    GOOGLE_API_KEY="AIzaSyDOjLjuJzlAEfN7VysS80tdPsGpsNiVZOI"
+    GOOGLE_API_KEY="AIzaSyATVShEBpb9rGD8wDQtkagzzL6nph6N398"
 
 # mongodb://localhost:27017
